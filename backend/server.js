603b32754e8f4c6c62f875d980dd5ff8bd30f552
acharyanisha1234 <acharyanisha2063gmail.com>
2026-06-10 11:42:24 +0545

    feat: ignore env file

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+
+connectDB();
+app.listen(5000, () => {
+    console.log(`Server is running on ${PORT}`);
+});
