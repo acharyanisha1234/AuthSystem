30fd9d50ce6853f97c13d1d32444436fb5fca49b
acharyanisha1234 <acharyanisha2063gmail.com>
2026-06-10 11:23:09 +0545

    feat(auth): add user authentication system using JWT
    - Added user registration with password hashing using bcrypt
    - Added user login with access and refresh token generation
    - Implemented refresh token functionality
    - Added secure HTTP-only cookie storage for refresh tokens
    - Implemented user logout by clearing refresh token cookie
    - Added validation and error handling for authentication routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-import  express from 'express' ; 
+import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -7,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 
-
+connectDB();
 app.listen(5000, () => {
     console.log(`Server is running on ${PORT}`);
 });
