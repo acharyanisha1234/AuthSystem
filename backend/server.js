5074aa7073793e738d473d0d0799456b1a548622
acharyanisha1234 <acharyanisha2063gmail.com>
2026-06-10 12:34:24 +0545

    resolve merge conflict in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
-<<<<<<< HEAD
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.js";
@@ -19,7 +18,7 @@ app.use(cookieParser());
 
 app.use('api/auth',authRoutes);
 app.use('/api/users',userRoutes);
-=======
+
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -27,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 
->>>>>>> b39426789d7b93c1c7da4d7dedfecac64666be93
+
 connectDB();
 app.listen(5000, () => {
     console.log(`Server is running on ${PORT}`);
